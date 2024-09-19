37fac7dd16370035b582fe1b430c8249320dea77
Colin Miller <ocolin@cruzio.com>
2024-09-19 13:53:36 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # Tarana
-Client for Tarana Cloud
+
+## Description
+
+This is a basic client for the Tarana API. It uses the EasySwagger library, but contains the Tarana swagger JSON file.
+
+## Usage
+
+### Environment variables
+
+You can specify parameters in the constructor, but if any are left out, these environment variables will be used instead.
+
+TARANA_API_TOKEN - Authentication token for API
+
+TARANA_HOST - URL of server API
+
+### Instantiate
+
+Create an instance of the Tarana object.
+
+```
+$tarana = new Ocolin\Tarana\Tarana();
+```
+
+#### Parameters
+
+$host: Name of the Tarana host server. If null, will use .env field.
+
+$api_key: Authentication token for server. If null, will use .env field.
+
+$api_file: Path to Swagger JSON file. If null, will use included file.
+
+$local: If true, it will try to load .env file in root directory.
+
+### Making a call
+
+```
+$output = $tarana->path( 
+    path: '/v1/network/regions',
+    data: [ 'count' => '10' ]
+);
+```
+
+#### Parameters
+
+$path: REQUIRED - Swagger call path, including named parameters.
+
+$data: Array of parameters name/values to use for URI path or body.
+
+$method: HTTP method. Defaults to GET.
---
+++ src/Tarana.php
@@ -0,0 +1,77 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace Ocolin\Tarana;
+
+use Exception;
+use GuzzleHttp\Exception\GuzzleException;
+use Ocolin\Env\EasyEnv;
+use Ocolin\EasySwagger\Swagger;
+
+class Tarana
+{
+
+    public Swagger $swagger;
+
+
+/* CONSTRUCTOR
+----------------------------------------------------------------------------- */
+
+    /**
+     * @param string|null $host     URL of UISP server.
+     * @param string|null $api_key  UISP API Token.
+     * @param string|null $api_file File path to UISP Swagger JSON file.
+     * @param bool $local Load local .env file for Environment variables. False if
+     *      they are being loaded by project implementing this one.
+     * @throws Exception
+     */
+    public function __construct(
+        ?string $host     = null,
+        ?string $api_key  = null,
+        ?string $api_file = null,
+        bool $local    = false
+    ) {
+        if( $local === true ) {
+            EasyEnv::loadEnv( path: __DIR__ . '/../.env' );
+        }
+        $host     = $host     ?? $_ENV['TARANA_HOST'];
+        $api_key  = $api_key  ?? $_ENV['TARANA_API_TOKEN'];
+        $api_file = $api_file ?? __DIR__ . '/api.v.3.0.1.json';
+
+        $this->swagger = new Swagger(
+                  host: $host,
+              api_file: $api_file,
+              base_uri: '',
+                 token: $api_key,
+            token_name: 'X-API-Key'
+        );
+    }
+
+
+
+/* QUERY API PATH
+----------------------------------------------------------------------------- */
+
+    /**
+     * @param string $path      Unaltered API path (including variable placeholders)
+     * @param string $method    HTTP method. Defaults to GET.
+     * @param array|null $data  Array of any parameters for the URI or body.
+     * @return object|array     Reply from server API.
+     * @throws GuzzleException
+     */
+    public function path(
+        string $path,
+        string $method = 'get',
+         array $data   = []
+    ) : object|array
+    {
+        $data = $data ?? [];
+        return $this->swagger->path(
+              path: $path,
+            method: $method,
+              data: $data
+        );
+    }
+
+}
